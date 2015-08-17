3e3e55233fdff76c8fcef100f85af9ac6f377d59
John Holliday <johnmholliday@gmail.com>
2015-08-16 23:22:04 -0500

    Add bower.json and package.json. for 4 libraries. Add gulpfile.js with watch to minify and concat JS.

---
+++ AngularJSWebApi/gulpfile.js
@@ -0,0 +1,39 @@
+﻿/// <binding BeforeBuild='scripts' ProjectOpened='watch' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+// include plug-ins
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+
+var config = {
+    //Include all js files but exclude any min.js files
+    src: ['app/**/*.js', '!app/**/*.min.js']
+}
+
+// Synchronously delete the output file(s)
+gulp.task('clean', function () {
+    del.sync(['app/all.min.js'])
+});
+
+// Combine and minify all files from the app folder
+gulp.task('scripts', ['clean'], function () {
+
+    return gulp.src(config.src)
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+      .pipe(gulp.dest('app/'));
+});
+
+//Set a default tasks
+gulp.task('default', ['scripts'], function () { });
+
+
+var watch = require('gulp-watch');
+gulp.task('watch', function () {
+    return gulp.watch(config.src, ['scripts']);
+});
