3f9269dae0dd2b936d06afec2c79ea34960a58b0
John Holliday <johnmholliday@gmail.com>
2015-08-23 21:38:35 -0500

    Ignore built files.

--- AngularJSWebApi/gulpfile.js
+++ AngularJSWebApi/gulpfile.js
@@ -1,7 +1,6 @@
 ﻿/// <binding BeforeBuild='scripts' ProjectOpened='watch' />
 /*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+  This combines and minifies app files. 
 */
 
 // include plug-ins
